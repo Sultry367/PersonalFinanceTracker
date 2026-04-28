cf688695fe819426ef3cf0cd04ef44cac1d9be4c
Sultry <andibabawi5@gmail.com>
2026-04-29 01:11:59 +1000

    Controllers that have Crud stuff and saving with _Context

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinanceTracker.Data;
+using PersonalFinanceTracker.Dtos.TransactionDtos;
+using PersonalFinanceTracker.Models;
+
+namespace PersonalFinanceTracker.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionController:ControllerBase
+{
+    private readonly AppDbContext _context;
+    
+    public TransactionController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllTransactions()
+    {
+        var transactions = await _context.Transaction.ToListAsync();
+        return Ok(transactions);
+    }
+
+    [HttpGet("user/{userId:guid}")]
+    public async Task<IActionResult> GetUserIdTransaction([FromRoute]Guid userId)
+    {
+        var transactions = await _context.Transaction.Where(x => x.UserId == userId).ToListAsync();
+        return Ok(transactions);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTransactionProduct([FromBody] CreateTransactionDto transactionDto)
+    {
+        var transactions = new Transactions()
+        {
+            Amount = transactionDto.Amount,
+            Category = transactionDto.Category,
+            Date = transactionDto.Date,
+            Description = transactionDto.Description,
+            IsSubscription = transactionDto.IsSubscription,
+            UserId = transactionDto.UserId,
+        };
+        await _context.Transaction.AddAsync(transactions);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUserIdTransaction),  new { userId = transactions.UserId }, transactions);
+    }
+
+    [HttpPut("{transactionId:guid}")]
+    public async Task<IActionResult> UpdateTransactionProduct([FromRoute]Guid transactionId, [FromBody] UpdateTransactionDto transactionDto)
+    {
+        var existingTransaction = await _context.Transaction.FirstOrDefaultAsync(y => y.Id == transactionId);
+        if (existingTransaction == null)
+            return NotFound(new { Message = $"Transaction id {transactionId} was not found or is invalid" });
+        existingTransaction.UserId = transactionDto.UserId;
+        existingTransaction.Amount = transactionDto.Amount;
+        existingTransaction.Category = transactionDto.Category;
+        existingTransaction.IsSubscription = transactionDto.IsSubscription;
+        existingTransaction.Date = transactionDto.Date;
+        existingTransaction.Description = transactionDto.Description;
+
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{transactionId:guid}")]
+    public async Task<IActionResult> DeleteTransactionProduct([FromRoute] Guid transactionId)
+    {
+        var deleteTransaction = await _context.Transaction.FindAsync(transactionId);
+        if (deleteTransaction == null)
+            return NotFound(new { message = $"Transaction id {transactionId} was not found or is invalid" });
+        _context.Transaction.Remove(deleteTransaction);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinanceTracker.Data;
+using PersonalFinanceTracker.Dtos.UserDtos;
+using PersonalFinanceTracker.Models;
+
+namespace PersonalFinanceTracker.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController:ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public UserController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var user = await _context.User.ToListAsync();
+        return Ok(user);
+    }
+
+    [HttpGet("user/{id:guid}")]
+    public async Task<IActionResult> GetUserById([FromRoute] Guid id)
+    {
+        var user = await _context.User.FindAsync(id);
+        if (user == null)
+            return NotFound(new { Message = $"User id {id} was not found or is invalid" });
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] CreateUserDto userDto)
+    {
+        var user = new Users()
+        {
+            FirstName = userDto.FirstName,
+            LastName = userDto.LastName,
+            Age = userDto.Age,
+        };
+        await _context.User.AddAsync(user);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+    }
+
+    [HttpPut("user/{id:guid}")]
+    public async Task<IActionResult> UpdateUser([FromRoute] Guid id, [FromBody] UpdateUserDto userDto)
+    {
+        var existingUser = await _context.User.FirstOrDefaultAsync(t => t.Id == id);
+        if(existingUser == null)
+            return NotFound(new { Message = $"User id {id} was not found or is invalid" });
+        existingUser.FirstName = userDto.FirstName;
+        existingUser.LastName = userDto.LastName;
+        existingUser.Age = userDto.Age;
+
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("user/{id:guid}")]
+    public async Task<IActionResult> DeleteUser([FromRoute] Guid id)
+    {
+        var deleteUser = await _context.User.FindAsync(id);
+        if(deleteUser == null)
+            return NotFound(new { Message = $"User id {id} was not found or is invalid" });
+        _context.User.Remove(deleteUser);
+        await _context.SaveChangesAsync();
+        return NoContent(); 
+    }
+}
