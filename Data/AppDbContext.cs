35dd9d54e9c79446a8e921fce5fe3e85666ed7dd
Sultry <andibabawi5@gmail.com>
2026-04-29 01:09:38 +1000

    Feat: AppDbContext stuff to save changes and also add identity stuff to make logging in or Authentication/Authorization safer

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinanceTracker.Models;
+
+namespace PersonalFinanceTracker.Data;
+
+public class AppDbContext:IdentityDbContext<ApplicationUser,ApplicationRole,string>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options) {}
+    
+    public DbSet<Transactions> Transaction { get; set; }
+    public DbSet<Users> User { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Users>()
+            .HasMany(u => u.Transactions)
+            .WithOne(t => t.User)
+            .HasForeignKey(y => y.UserId)
+            .IsRequired();
+    }
+}
---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PersonalFinanceTracker.Data;
+
+public class ApplicationUser:IdentityUser
+{
+    
+}
